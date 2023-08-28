1da452cbc29c01aa052933ad64b7dabe769cf460
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-08-28 16:10:33 -0300

    ajustes for deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
-import NextAuth from "next-auth"
-import type { NextAuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
     providers: [
       CredentialsProvider({
         name: "Credentials",
