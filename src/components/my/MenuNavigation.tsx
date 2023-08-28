04bb5665a17e4c0cf6d26cb1a280657f801281b8
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-08-28 19:34:06 -0300

    nav menu

--- src/components/my/MenuNavigation.tsx
+++ src/components/my/MenuNavigation.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import { useState } from 'react';
 import { ButtonTheme } from './ButtonTheme';
 import SigninButton from './SigninButton';
 import { Separator } from '../ui/separator';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { Input } from "@/components/ui/input"
+import { MenuCategorie } from './MenuCategorie';
 
 export default function MenuNavigation() {
-    const [value, setValue] = useState(0);
 
     return (
         <>
@@ -20,16 +16,26 @@ export default function MenuNavigation() {
                 <div>
                     LOGO
                 </div>
-                <div className='flex items-center justify-center'>
-                    MENU CENTRAL
+                <div className='flex items-center justify-center gap-6'>
+                    <div>
+                        <MenuCategorie />
+                    </div>
+                    <div>
+                        <Input placeholder='Pesquise...' />
+                    </div>
                 </div>
                 <div className='flex gap-6  justify-end'>
+                    <Button variant={"outline"}>
+                        <Link href={"https://allan-dev-portfolio.vercel.app/"} target='_blank'>
+                            Sobre mim
+                        </Link>
+                    </Button>
                     <SigninButton />
                     <ButtonTheme />
                 </div>
             </div>
             <div>
-                <Separator/>
+                <Separator />
             </div>
         </>
     )
