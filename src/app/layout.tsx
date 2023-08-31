53ac9de2adb89b621ecd75a2a4daa4908de9907f
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-08-31 18:35:57 -0300

    init menu mobile (sheet)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from "@/components/theme-provider"
 import MenuNavigation from '@/components/my/MenuNavigation'
+import { MobileNavigation } from '@/components/my/MobileNavigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +24,8 @@ export default function RootLayout({
         <Provider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className='container'>
-              <nav className='my-10'><MenuNavigation /></nav>
+              <nav className='sm:hidden my-10'><MobileNavigation /></nav>
+              <nav className='hidden sm:block my-10'><MenuNavigation /></nav>
               {children}
             </div>
           </ThemeProvider>
