1617409850597249f1adac75367dddfc36396535
Allan Batista <allanbatista@MacBook-Air-de-Allan.local>
2023-09-01 12:12:50 -0300

    news components

--- src/app/admin/components/FormPost.tsx
+++ src/app/admin/components/FormPost.tsx
@@ -1,80 +1,74 @@
 "use client";
-
-import { Post } from "@/app/common/types/post.types";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { UserCircleIcon } from "lucide-react";
-import dynamic from "next/dynamic";
 import { useState } from "react";
-const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+import dynamic from "next/dynamic";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Post } from "@/app/common/types/post.types";
 import 'react-quill/dist/quill.snow.css';
+import SelectThread from "./SelectThread";
+
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
 export default function FormPost({
     title, threadId, subtitle, file, isActive, content, userId
 }: Post) {
-    let toolbarOptions = [
-        ['bold', 'italic'], ['link', 'image'],
-        ['bold', 'italic', 'underline', 'strike'],
-        ['blockquote', 'code-block'],
-        [{ 'header': 1 }, { 'header': 2 }],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-        [{ 'script': 'sub' }, { 'script': 'super' }],
-        [{ 'indent': '-1' }, { 'indent': '+1' }],
-        [{ 'direction': 'rtl' }],
-        [{ 'size': ['small', false, 'large', 'huge'] }],
-        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-        [{ 'color': [] }, { 'background': [] }],
-        [{ 'font': [] }],
-        [{ 'align': [] }],
-        ['clean']
-    ];
-    const quillModules = {
-        toolbar: toolbarOptions
-    }
-
     const [selectedFile, setSelectedFile] = useState(null);
+    const [previewUrl, setPreviewUrl] = useState('');
 
-    // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     const file = event.target.files[0];
-    //     setSelectedFile(file);
-    // };
+    const handleFileChange = (e: any) => {
+        const file = e.target.files[0];
+        setSelectedFile(file);
 
-    const uploadFile = () => {
-        if (selectedFile) {
-            console.log("Arquivo selecionado:", selectedFile);
-        } else {
-            alert("Selecione um arquivo antes de fazer upload.");
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setPreviewUrl(reader.result as string);
+            };
+            reader.readAsDataURL(file);
         }
     };
 
     return (
         <>
-            {/* <div className="col-span-full">
-                <label htmlFor="uploadFile" className="block text-sm font-medium leading-6 text-gray-900">
-                    Photo
-                </label>
-                <input type="file" id="uploadFile" name="file" accept=".jpg, .jpeg, .png, .pdf" onChange={handleFileChange} />
-                <div className="mt-2 flex items-center gap-x-3">
-                    <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
-                    <Button
-                        type="button"
-                        variant="outline"
-                        onClick={uploadFile}
-                    >
-                        Adicionar capa
-                    </Button>
-                </div>
-            </div> */}
+            <div className="mb-10">
+                <Label className="mb-2 scroll-m-20 text-2xl font-semibold tracking-tight">Thread</Label>
+                <SelectThread />
+            </div>
+            <div className="mb-10">
+                <Label className="mb-2 scroll-m-20 text-2xl font-semibold tracking-tight">Capa</Label>
+                <Input
+                    type="file"
+                    onChange={handleFileChange}
+                    value={file}
+                    accept="image/*"
+                    id="picture"
+                    required
+                    className="cursor-pointer"
+                />
+                {previewUrl && (
+                    <div className="mt-2">
+                        <label className="text-muted-foreground">Prévia</label>
+                        <div className="flex justify-center">
+                            <img src={previewUrl} alt="Preview" className="max-h-32 mt-2" />
+                        </div>
+                    </div>
+                )}
+            </div>
             <div className="mb-10">
                 <Label className="mb-2 scroll-m-20 text-2xl font-semibold tracking-tight">Título</Label>
                 <Input
                     placeholder={title}
                 />
             </div>
+            <div className="mb-10">
+                <Label className="mb-2 scroll-m-20 text-2xl font-semibold tracking-tight">Subtitulo</Label>
+                <Input
+                    placeholder={subtitle}
+                />
+            </div>
             <div>
                 <Label className="mb-2 scroll-m-20 text-2xl font-semibold tracking-tight">Conteúdo</Label>
-                <ReactQuill modules={quillModules} theme="snow" value={content} />
+                <ReactQuill modules={{ toolbar: true }} theme="snow" value={content} />
             </div>
         </>
     )
